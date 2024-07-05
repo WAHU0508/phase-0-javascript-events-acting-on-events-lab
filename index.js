2889a4bb4181b0ad6e058a319c274020c631176b
WAHU0508 <sharongikenye@gmail.com>
2024-07-05 08:24:04 +0300

    Move Dodger up and down, left and right

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-const dodger = document.getElementById("dodger")
+const dodger = document.getElementById("dodger");
 
 function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
 
-    const leftNumbers = dodger.style.left.replace("px", "")
-    const left = parseInt(leftNumbers, 10)
     if (left > 0) {
-        dodger.style.left = `${left - 1}px`
+        dodger.style.left = `${left - 1}px`;
     }
-
 }
-
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-        moveDodgerLeft()
-
-    }
-
-});
-
-
-
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "")
-    const left = parseInt(leftNumbers, 10)
-
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
 
     if (left < 360) {
-
         dodger.style.left = `${left + 1}px`
-
     }
 }
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowRight") {
-        moveDodgerRight()
+function moveDodgerUp() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "")
+    const bottom = parseInt(bottomNumbers, 10)
+    if (bottom < 380) {
+        dodger.style.bottom = `${bottom + 1}px`
     }
-})
+}
+function moveDodgerDown() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "")
+    const bottom = parseInt(bottomNumbers, 10)
+    if (bottom > 0) {
+        dodger.style.bottom = `${bottom - 1}px`
+    }
+}
+//EventListener
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+});
+
